8efde3bf35c43e92b72e4c715107bc7208d1d9b0
MatiasGomez <gomezmati@hotmail.com>
2023-01-11 21:25:04 -0300

    agregando itemcount al proyecto

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import ItemCount from "../ItemCount";
+import "./styles.css";
 
-const ItemDetail = ({detail}) => {
-    console.log(detail);
+const ItemDetail = ({ detail }) => {
+
+    const [quantity, setQuantity] = useState(0)
+
+    const onAdd = (cantidad) => {
+        console.log(`Se agregó una cantidad de productos: ${cantidad}`)
+        setQuantity(cantidad)
+    }
+
+    console.log(detail.title);
     return (
-        <div>{detail.description}</div>
-    )
-}
+        <div className="detail-container">
+            <img className="detail-img" src={detail.image} alt="detail" />
+            <aside className="detail-aside">
+                <h2>{detail.title}</h2>
+                <h5>{detail.description}</h5>
+                {
+                    quantity === 0 ?
+                    <ItemCount 
+                        stock={20} 
+                        initial={1} 
+                        onAdd={onAdd}    
+                    />
+                    :
+                    <button className="btn btn-primary p-2">
+                        <Link to="/cart">
+                            Go cart
+                        </Link>
+                    </button>
+                }
+            </aside>
+        </div>
+    );
+};
 
-export default ItemDetail
+export default ItemDetail;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <Link class="navbar-brand" to="/">Home</Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <Link className="navbar-brand" to="/">Home</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <Link class="nav-link active" aria-current="page" to="/category/termos">Termos</Link>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/category/termos">Termos</Link>
           </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/category/mates">Mates</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/category/mate">Mates</Link>
           </li>
-          <li class="nav-item">
-            <Link class="nav-link" to="/category/accesorios">Accesorios</Link>
+          <li className="nav-item">
+            <Link className="nav-link" to="/category/accesorios">Accesorios</Link>
           </li>
         </ul>
         <CartWidget/>
-        <span class="navbar-text">
+        <span className="navbar-text">
           2
         </span>
       </div>
